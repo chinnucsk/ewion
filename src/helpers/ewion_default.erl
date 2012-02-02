b5931290075a3cede8879e596ff1134ba94a5c8f
Richard van Stark <richard@8protons.com>
2012-02-02 19:19:19 +0400

    Fix ewion_default

--- src/helpers/ewion_default.erl
+++ src/helpers/ewion_default.erl
@@ -4,6 +4,6 @@
 
 -export([handle_request/2]).
 
-handle_request(Pid, Env) ->
+handle_request(_Pid, Env) ->
     Headers = ewion_h:gv(headers, Env),
-    Pid ! ewion:ok("Config not found for host: " ++ binary_to_list(ewion_h:gv('Host', Headers))).
+    ewion:ok("Config not found for host: " ++ binary_to_list(ewion_h:gv('Host', Headers))).
