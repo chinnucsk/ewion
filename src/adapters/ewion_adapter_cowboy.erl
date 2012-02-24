be1991c5accd3abc779016aa077f39360daadeee
Richard van Stark <richard@8protons.com>
2012-02-24 19:26:03 +0400

    Fix error with GET and POST args. New API (ok, Response)

--- src/adapters/ewion_adapter_cowboy.erl
+++ src/adapters/ewion_adapter_cowboy.erl
@@ -26,22 +26,29 @@ handle_http(Req, ConfigModule) ->
     Env = get_env(Req),
 
     case rpc:call(Node, Module, handle_request, [{self(), node()}, Env]) of
-        chunked ->
+        {ok, chunked} ->
             handle_chunked_response(Req);
 
-        {Status, Headers, Data} ->
+        {ok, {Status, Headers, Data}} ->
             {ok, Req2} = cowboy_http_req:reply(Status, Headers, Data, Req),
             {ok, Req2, ConfigModule}
     end.
 
 get_env(Req) ->
+    Method = element(1, cowboy_http_req:method(Req)),    
     [
         {method, element(1, cowboy_http_req:method(Req))},
         {path, binary_to_list(element(1, cowboy_http_req:raw_path(Req)))},
-        {args, element(1, cowboy_http_req:qs_vals(Req))},
+        {args, get_req_args(Method, Req)},
         {cookies, element(1, cowboy_http_req:cookies(Req))},
         {headers, element(1, cowboy_http_req:headers(Req))}
     ].
+
+get_req_args('GET', Req) ->
+    element(1, cowboy_http_req:qs_vals(Req));
+
+get_req_args('POST', Req) ->
+    element(1, cowboy_http_req:body_qs(Req)).
 
 %% ===================================================================
 %% Handle Chunked Response
